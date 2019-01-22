5fca379dbb6907f6793fafcb29764e76f967b930
BrunEmilien <38348712+BrunEmilien@users.noreply.github.com>
2019-01-22 16:54:57 +0100

    Commentaires
    ajouts des commentaires dans style.css, header.php et script.js

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,12 +1,12 @@
-$(function(){
+$(function(){ /*Si le DOM est bien chargé alors:*/
 
-	function actualise(){
-		if($(window).width() < 860 ){
-			$('#buttonMenu').show();
+	function actualise(){ /*Cette fonction permet de changer les composant pour s'adapter a l'écran de l'uttilisateur*/
+		if($(window).width() < 860 ){/*Si l'écran est plus petit qu'une tablette*/
+			$('#buttonMenu').show(); /*alors on affiche le menu dérroullant, on cache les liens et on rend le logo plus gros*/
 			$('.submenu').css('display', 'none');
 			$('#logo').css('width','60%');
 			$('#logo').css('height','60%');
-		} else {
+		} else {/*Sinon on cache le menu dérroullant, on affiche la navbar et le logo prend ca taille habituelle*/
 			$('#buttonMenu').hide();
 			$('.submenu').css('display', 'block');
 			$('#logo').css('width','15%');
@@ -14,17 +14,17 @@ $(function(){
 		}	
 	}
 
-	actualise();
+	actualise();/*Au commencement du code, on actualise l'affichage*/
 
-	$('#buttonMenu').click(function(){
-		if($('.submenu').css('display') == 'none'){
+	$('#buttonMenu').click(function(){ /*Si on clique sur le bouton du menu déroullant*/
+		if($('.submenu').css('display') == 'none'){/*Alors on inverse l'état des liens de la navbar*/
 			$('.submenu').css('display', 'block');
 		} else {
 			$('.submenu').css('display', 'none');
 		}
 	});
 	
-	$(window).resize(function(){
+	$(window).resize(function(){/*Si la page est redimmensionné, on l'actualise*/
 		actualise();
 	});
 });
--- resources/views/headers/header.blade.php
+++ resources/views/headers/header.blade.php
@@ -6,10 +6,13 @@
 <html>
     <body>
         <header>
+
+            {{-- Premiere nav-barre la barre bleu qui reste tout en haut de l'écran--}}
             <nav class="navbar navbar-expand-md sticky-top navbar-dark headNav" id="first-head">
                 <ul class="navbar-nav ml-auto">
                     <li class="navbar-item" id="user-name">
                         @php
+                            /*Affichage du nom et prenom si connecté sinon affiche le lien vers la connection*/
                             if (isset($_SESSION['userloged']) && isset($_SESSION['user_name']) && isset($_SESSION['user_last-name']) && $_SESSION['userloged'] == true)
                             {
                                 echo '<a href="../../personnel" class="nav-link">'. $_SESSION['user_name'] . $_SESSION['user_last-name'].' <i class="far fa-user"></i></a>';
@@ -21,7 +24,9 @@
                     </li>
                     <li class="navbar-item" id="user-panier">
                         @php
+                            /*Affichage du pannier si l'uttilisateur est connecté*/
                             if (isset($_SESSION['userloged']) && isset($_SESSION['user_pseudo']) && $_SESSION['userloged']){
+                                /*Si l'uttilisateur a deja un pannier on le reprend sinon on lui dit qu'il est vide*/
                                 if(isset($_COOKIE['panier'])){
                                     echo '<a href="../../panier" class="nav-link">Pannier:'.$_COOKIE['panier'].' €</a>';
                                 } else {
@@ -34,10 +39,13 @@
                     </li>
                 </ul>
             </nav>
+
+            {{-- Deuxieme barre du header qui n'affiche que le logo. cliquer sur le logo mene a la page d'index --}}
             <div class="headLogo" id="second-head">
                 <a href="../../home"><img id="logo" src="resources/assets/Images/logo.png" alt="logo"></a>
             </div>
 
+            {{-- Troisieme et derniere barre du header bleu elle aussi elle contient un menu dérroullant menant aux différentes section du sites. Si l'écran est grand alors on cache le menu dérroullant. ca se passe dans script.js--}}
             <nav class="navbar navbar-expand-md navbar-dark headNav row" id="third-head">
                 <ul class="navbar-nav" id="menu">
                     <button type="button" class="btn btn-dark" id="buttonMenu"><i class="fas fa-bars fa-3x" id="menuTop"></i></button>
@@ -59,57 +67,3 @@
         </header>
     </body>
 </html>
-
-
-
-
-
-
-{{--<nav class="navbar navbar-expand-md sticky-top navbar-dark" style="background-color: steelblue;" id="navbar">
-    <a class="navbar-brand h1 mb-0" href="../../index.php">
-        <img src="images/logo.png" width="30" height="30" alt="logo">
-        BDE EXIA NANCY
-    </a>
-    <button id="navbarTogler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbartab" aria-controls="#navbartab" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse mr-auto" id="navbartab">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link active" href="../../index.php#aboutUs">
-                    Boutique 
-                </a>
-            </li>
-            <li class="navbar-item">
-                <a class="nav-link" href="../../index.php#services">
-                    Events
-                </a>
-            </li>
-            <li class="navbar-item">
-                <a class="nav-link" href="../../index.php#contact">
-                    Perso
-                </a>
-            </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="navbar-item">
-
-                @php
-                    session_start();
-
-                    if (isset($_SESSION['userloged']) && isset($_SESSION['user_pseudo']) && $_SESSION['userloged'] == true)
-                    {
-                        echo '<span class="navbar-text mr-2">Bonjour '. $_SESSION['user_pseudo'] .' !</span></li><li class="navbar-item">';
-                        echo '<a href="../../deconnexion" class="nav-link">Déconnexion</a>';
-                    }
-                    else
-                    {
-                        echo '<a href="../../connexion" class="nav-link">Connexion</a>';
-                    }
-                @endphp
-            </li>
-        </ul>
-    </div>
-</nav>
---}}
