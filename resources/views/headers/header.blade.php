ed25433d1b4ca33f473dc6d4a7368b9926099906
BrunEmilien <38348712+BrunEmilien@users.noreply.github.com>
2019-01-24 10:05:19 +0100

    liens staique

--- resources/views/headers/header.blade.php
+++ resources/views/headers/header.blade.php
@@ -12,8 +12,8 @@
                     /*Affichage du nom et prenom si connecté sinon affiche le lien vers la connection*/
                     if (isset($_SESSION['userloged']) && isset($_SESSION['user_name']) && isset($_SESSION['user_last-name']) && $_SESSION['userloged'] == true)
                     {
-                        echo '<a href="../../personnel" class="nav-link">'. $_SESSION['user_name'] . $_SESSION['user_last-name'].'   <i class="far fa-user"></i></a>';
-                        echo '<a href="../../deconnexion" class="nav-link">Déconnexion</a>';
+                        echo '<a href="http://localhost/projetweb/server.php/personnel" class="nav-link">'. $_SESSION['user_name'] . $_SESSION['user_last-name'].'   <i class="far fa-user"></i></a>';
+                        echo '<a href="http://localhost/projetweb/server.php/deconnexion" class="nav-link">Déconnexion</a>';
                     } else {
                         echo '<a href="http://localhost/projetweb/server.php/connexion" class="nav-link">Connexion   <i class="far fa-user"></i></a>';
                     }
@@ -25,12 +25,12 @@
                     if (isset($_SESSION['userloged']) && isset($_SESSION['user_pseudo']) && $_SESSION['userloged']){
                         /*Si l'uttilisateur a deja un pannier on le reprend sinon on lui dit qu'il est vide*/
                         if(isset($_COOKIE['panier'])){
-                            echo '<a href="../../panier" class="nav-link">Pannier:'.$_COOKIE['panier'].' €   <i class="fas fa-shopping-basket"></i></a>';
+                            echo '<a href="http://localhost/projetweb/server.php/panier" class="nav-link">Pannier:'.$_COOKIE['panier'].' €   <i class="fas fa-shopping-basket"></i></a>';
                         } else {
-                            echo '<a href="../../panier" class="nav-link">Panier: 00.00 €   <i class="fas fa-shopping-basket"></i></a>';
+                            echo '<a href="http://localhost/projetweb/server.php/panier" class="nav-link">Panier: 00.00 €   <i class="fas fa-shopping-basket"></i></a>';
                         }
                     } else {
-                        echo '<a href="../../panier" class="nav-link">Panier:  00.00 €   <i class="fas fa-shopping-basket"></i></a>';
+                        echo '<a href="http://localhost/projetweb/server.php/panier" class="nav-link">Panier:  00.00 €   <i class="fas fa-shopping-basket"></i></a>';
                     }
                  @endphp
             </li>
@@ -39,7 +39,7 @@
 
     {{-- Deuxieme barre du header qui n'affiche que le logo. cliquer sur le logo mene a la page d'index --}}
     <div class="headLogo" id="second-head">
-        <a href="../../home"><img id="logo" src="http://localhost/projetweb/resources/assets/Images/logo.png" alt="logo"></a>
+        <a href="http://localhost/projetweb/server.php/"><img id="logo" src="http://localhost/projetweb/resources/assets/Images/logo.png" alt="logo"></a>
     </div>
 
     {{-- Troisieme et derniere barre du header bleu elle aussi elle contient un menu dérroullant menant aux différentes section du sites. Si l'écran est grand alors on cache le menu dérroullant. ca se passe dans script.js--}}
@@ -47,16 +47,16 @@
         <ul class="navbar-nav" id="menu">
             <button type="button" class="btn btn-dark" id="buttonMenu"><i class="fas fa-bars fa-2x" id="menuTop"></i></button>
             <li class="navbar-item submenu">
-                <a href="../../event" class="nav-link"><i class="fas fa-users"></i>   EVENEMENTS</a>
+                <a href="http://localhost/projetweb/server.php/event" class="nav-link"><i class="fas fa-users"></i>   EVENEMENTS</a>
             </li>
             <li class="navbar-item submenu">
-                <a href="../../event" class="nav-link"><i class="far fa-lightbulb"></i>   BOITE A IDEES</a>
+                <a href="http://localhost/projetweb/server.php/event" class="nav-link"><i class="far fa-lightbulb"></i>   BOITE A IDEES</a>
             </li>
             <li class="navbar-item submenu">
-                <a href="../../event" class="nav-link"><i class="fas fa-shopping-cart"></i>   BOUTIQUE</a>
+                <a href="http://localhost/projetweb/server.php/event" class="nav-link"><i class="fas fa-shopping-cart"></i>   BOUTIQUE</a>
             </li>
             <li class="navbar-item submenu">
-                <a href="../../event" class="nav-link"><i class="far fa-envelope-open"></i>   CONTACT</a>
+                <a href="http://localhost/projetweb/server.php/event" class="nav-link"><i class="far fa-envelope-open"></i>   CONTACT</a>
             </li>
         </ul>
     </nav>
